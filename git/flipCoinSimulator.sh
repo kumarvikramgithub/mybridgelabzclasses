54e96fcae68ce0a91277c6c545538d97f70cfdad
kumar vikram <vikram2015cse01@gmail.com>
2021-10-09 23:15:07 +0530

    Refactor - flip coin simulator to see who win when matched gone tie

--- git/flipCoinSimulator.sh
+++ git/flipCoinSimulator.sh
@@ -1,3 +1,5 @@
+hwin=0;
+twin=0;
 #
 #read -p How many times You want to flip a coin: " n;
 #for (( i=1;i<=$n;i++ ))
@@ -10,24 +12,54 @@
 #		echo Tails Win";
 #	fi;
 #done;
-hwin=0;
-twin=0;
-while [ $hwin -ne 21 ] && [ $twin -ne 21 ]
-do
-        res=$((RANDOM%2));
+function getResult () {
+	 res=$((RANDOM%2));
         if [ $res -eq 1 ]
         then
-		hwin=$(($hwin+1));
-                echo "Heads Win -> $hwin";
+                #hwin=$(($hwin+1));
+                echo 1;
         else
- 		twin=$(($twin+1));
-                echo "Tails Win -> $twin";
+                #twin=$(($twin+1));
+                echo 0;
         fi;
+
+}
+
+while [ $hwin -ne 21 ] && [ $twin -ne 21 ]
+do
+       res=$( getResult );
+      	if [ $res -eq 1 ]
+	then
+		hwin=$(($hwin+1));
+	else
+		twin=$(($twin+1));
+	fi
 done;
 
 if [ $hwin -eq $twin ]
 then
         echo "Match got Tie";
+	tie=0;
+	while [ $tie -eq 0 ]
+	do
+		res=$( getResult );
+        	if [ $res -eq 1 ]
+        	then
+                	hwin=$(($hwin+1));
+        	else
+                	twin=$(($twin+1));
+        	fi
+
+		if [ $(($hwin-$twin)) -eq 2 ]
+		then
+			echo "Heads win Now By 2";
+                        tie=1;
+		elif [ $(($twin-$hwin)) -eq 2 ]
+		then
+			echo "Tails win Now By 2";
+			tie=1;
+		fi;
+	done
 elif [ $hwin -gt $twin ]
 then
         winby=$(($hwin-$twin));
