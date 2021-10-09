6b6990019abf9052ed62d8d71983d4ac1a4d88ce
kumar vikram <vikram2015cse01@gmail.com>
2021-10-09 22:49:28 +0530

    Refactor - flip coin simulator to see who win first 21 times

--- git/flipCoinSimulator.sh
+++ git/flipCoinSimulator.sh
@@ -1,12 +1,39 @@
 #
-read -p "How many times You want to flip a coin: " n;
-for (( i=1;i<=$n;i++ ))
+#read -p How many times You want to flip a coin: " n;
+#for (( i=1;i<=$n;i++ ))
+#do
+#	res=$((RANDOM%2));
+#	if [ $res -eq 1 ]
+#	then
+#		echo Heads Win";
+#	else
+#		echo Tails Win";
+#	fi;
+#done;
+hwin=0;
+twin=0;
+while [ $hwin -ne 21 ] && [ $twin -ne 21 ]
 do
-	res=$((RANDOM%2));
-	if [ $res -eq 1 ]
-	then
-		echo "Heads Win";
-	else
-		echo "Tails Win";
-	fi;
+        res=$((RANDOM%2));
+        if [ $res -eq 1 ]
+        then
+		hwin=$(($hwin+1));
+                echo "Heads Win -> $hwin";
+        else
+ 		twin=$(($twin+1));
+                echo "Tails Win -> $twin";
+        fi;
 done;
+
+if [ $hwin -eq $twin ]
+then
+        echo "Match got Tie";
+elif [ $hwin -gt $twin ]
+then
+        winby=$(($hwin-$twin));
+        echo "Heads Win by $winby";
+
+else
+	winby=$(($twin-$hwin));
+	echo "Tails Win by $winby";
+fi
