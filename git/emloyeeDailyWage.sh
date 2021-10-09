7c8665ff164c06f3b05036aa6edb30778edad21a
kumar vikram <vikram2015cse01@gmail.com>
2021-10-09 17:37:50 +0530

    Refactor - emmloyee Dail wage -- calculating wage for 20 day in a month

--- git/emloyeeDailyWage.sh
+++ git/emloyeeDailyWage.sh
@@ -2,7 +2,8 @@
 wage_per_hour=20;
 full_day_hour=8;
 part_time_day_hour=4;
-isPresent=$((RANDOM%3));
+max_in_month_work=20;
+#isPresent=$((RANDOM%3));
 #if [ $isPresent -eq 1 ]
 #then
 	#wages=$(($wage_per_hour*$full_day_hour));
@@ -14,16 +15,28 @@ isPresent=$((RANDOM%3));
 #else
 	#echo Wage = 0 Re. ";
 #fi
-case $isPresent in
-	1)
-        wages=$(($wage_per_hour*$full_day_hour));
-        echo "Wage = $wages Re. ";
-	;;
-	2)
-	wages=$(($wage_per_hour*$part_time_day_hour));
-        echo "Part Wage = $wages Re. ";
-	;;
-	*)
-	echo "Wage = 0 Re. ";
-	;;
-esac;
+total_wages=0;
+total_hour_work=0;
+for (( i=1;i<=20;i++ ))
+do
+	isPresent=$((RANDOM%3));
+	#wages=0;
+	work_hour=0;
+	case $isPresent in
+		1)
+        	wage=$(($wage_per_hour*$full_day_hour));
+        	work_hour=8;
+		;;
+		2)
+		wage=$(($wage_per_hour*$part_time_day_hour));
+        	 work_hour=4;
+		;;
+		*)
+		wage=0;
+		work_hour=0;
+		;;
+	esac;
+	total_wages=$(($total_wages+$wage));
+	total_hour_work=$(($total_hour_work+$work_hour));
+done
+echo "Total Wages = $total_wages for $total_hour_work Hour Work.";
