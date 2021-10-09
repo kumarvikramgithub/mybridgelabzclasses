278b1fda65fbe89b948f411c3e71be5e5243ca18
kumar vikram <vikram2015cse01@gmail.com>
2021-10-09 22:33:52 +0530

    Refactor - flip coin simulator to n times flipping

--- git/flipCoinSimulator.sh
+++ git/flipCoinSimulator.sh
@@ -1,8 +1,12 @@
 #
-res=$((RANDOM%2));
-if [ $res -eq 1 ]
-then
-	echo "Heads Win";
-else
-	echo "Tails Win";
-fi;
+read -p "How many times You want to flip a coin: " n;
+for (( i=1;i<=$n;i++ ))
+do
+	res=$((RANDOM%2));
+	if [ $res -eq 1 ]
+	then
+		echo "Heads Win";
+	else
+		echo "Tails Win";
+	fi;
+done;
