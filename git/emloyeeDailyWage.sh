d8fa4698c7af7f1702e177ab755d8b9e02d4bc75
kumar vikram <vikram2015cse01@gmail.com>
2021-10-09 16:55:20 +0530

    Refactor - emmloyee Dail wage -- add part time feature

--- git/emloyeeDailyWage.sh
+++ git/emloyeeDailyWage.sh
@@ -1,11 +1,16 @@
 #
 wage_per_hour=20;
 full_day_hour=8;
-isPresent=$((RANDOM%2));
+part_time_day_hour=4;
+isPresent=$((RANDOM%3));
 if [ $isPresent -eq 1 ]
 then
 	wages=$(($wage_per_hour*$full_day_hour));
 	echo "Wage = $wages Re. ";
+elif [ $isPresent -eq 2 ]
+then
+        wages=$(($wage_per_hour*$part_time_day_hour));
+        echo "Part Wage = $wages Re. ";
 else
 	echo "Wage = 0 Re. ";
 fi
